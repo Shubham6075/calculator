30fbd8a9d010697a920400adfef5b1434977eff3
Shubham Rathee <122021456+Shubham6075@users.noreply.github.com>
2023-01-24 08:33:24 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,68 @@
+
+import java.io.*;
+import java.lang.*;
+import java.lang.Math;
+import java.util.Scanner;
+public class Calculator {
+
+	public static void main(String[] args)
+	{
+		double num1, num2;
+
+		System.out.println(" Welcome to Calculator ");
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the numbers");
+
+		num1 = sc.nextDouble();
+
+		num2 = sc.nextDouble();
+
+		System.out.println("Enter the operator (+,-,*,/)");
+
+		char op = sc.next().charAt(0);
+
+		double o = 0;
+
+		switch (op) {
+
+			case '+':
+
+				o = num1 + num2;
+
+				break;
+
+			case '-':
+
+				o = num1 - num2;
+
+				break;
+
+			case '*':
+
+				o = num1 * num2;
+
+				break;
+
+			case '/':
+
+				o = num1 / num2;
+
+				break;
+
+			default:
+
+				System.out.println("You enter wrong input");
+
+				break;
+		}
+
+		System.out.println("The final result:" );
+
+		System.out.println();
+
+		System.out.println(num1 + " " + op + " " + num2
+				+ " = " + o);
+	}
+}
